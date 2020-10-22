0d976ed3c35ef0c324d1f3d822a0db4c85c4f871
haydenanderl33 <hayden.anderl33@gmail.com>
2020-10-21 19:33:55 -0600

    nesting in progress

--- higher-order.js
+++ higher-order.js
@@ -21,8 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+let evenNumbers  = mixedNumbers.filter(e => e % 2 === 0)
 
 
 ////////// PROBLEM 2 //////////
@@ -44,7 +43,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(e => e * 1.07)
 
 
 
@@ -63,7 +62,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((accumulator, currentValue) => accumulator + currentValue)
 
 
 
@@ -89,7 +88,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(e => e.CP > 200 )
 
 
 
@@ -106,7 +105,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map((e) => e.price + (e.price * e.tax))
 
 
 
@@ -126,6 +125,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal1 = purchases.filter((e) => e.owner === 'Bob' )
+let bobsTotal2 = bobsTotal1.map((e) => e.price )
+let bobsTotal = bobsTotal2.reduce((acc,curr) => acc + curr )
 
 
